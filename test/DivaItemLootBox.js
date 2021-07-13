ce05931590cbcec9eb20ac18e08355147639747f
Anton Schippmann <cryptoyak@diva.cards>
2021-07-13 22:12:11 +0900

    fix hidden events in library

--- test/DivaItemLootBox.js
+++ test/DivaItemLootBox.js
@@ -1,11 +1,11 @@
 /* libraries used */
 
-const { expect, assert } = require('chai');
-const { ethers } = require('hardhat'); // eslint-disable-line
+const { expect, assert } = require("chai");
+const { ethers } = require("hardhat"); // eslint-disable-line
 
-const setup = require('../lib/setupItems');
-const vals = require('../lib/valuesCommon');
-const testVals = require('../lib/testValuesCommon');
+const setup = require("../lib/setupItems");
+const vals = require("../lib/valuesCommon");
+const testVals = require("../lib/testValuesCommon");
 
 /* Useful aliases */
 const toBN = ethers.BigNumber.from;
@@ -15,7 +15,7 @@ let factory;
 let lootBox;
 let proxy;
 let owner;
-let userA;   // eslint-disable-line
+let userA; // eslint-disable-line
 let userB;
 let proxyForOwner;
 let _others; // eslint-disable-line
@@ -25,21 +25,23 @@ let _others; // eslint-disable-line
 // Not a function, the fields of the TransferSingle event.
 
 const TRANSFER_SINGLE_FIELDS = [
-  { type: 'address', name: '_operator', indexed: true },
-  { type: 'address', name: '_from', indexed: true },
-  { type: 'address', name: '_to', indexed: true },
-  { type: 'uint256', name: '_id' },
-  { type: 'uint256', name: '_amount' },
+  { type: "address", name: "_operator", indexed: true },
+  { type: "address", name: "_from", indexed: true },
+  { type: "address", name: "_to", indexed: true },
+  { type: "uint256", name: "_id" },
+  { type: "uint256", name: "_amount" },
 ];
 
 // Not a function, the keccak of the TransferSingle event.
 //
-const XferSingleInterface = new ethers.utils.Interface([{
-  name: 'TransferSingle',
-  type: 'event',
-  inputs: TRANSFER_SINGLE_FIELDS,
-}]);
-const TRANSFER_SINGLE_SIG = XferSingleInterface.getEventTopic('TransferSingle');
+const XferSingleInterface = new ethers.utils.Interface([
+  {
+    name: "TransferSingle",
+    type: "event",
+    inputs: TRANSFER_SINGLE_FIELDS,
+  },
+]);
+const TRANSFER_SINGLE_SIG = XferSingleInterface.getEventTopic("TransferSingle");
 // const TRANSFER_SINGLE_SIG = web3.eth.abi.encodeEventSignature({
 //   name: 'TransferSingle',
 //   type: 'event',
@@ -64,7 +66,7 @@ const totalEventTokens = (receipt, recipient) => {
         TRANSFER_SINGLE_FIELDS,
         raw.data,
         // Exclude event signature hash from topics that we process here.
-        raw.topics.slice(1),
+        raw.topics.slice(1)
       );
       // Make sure the address that we are watching got the tokens.
       // Burnt tokens go to address zero, for example
@@ -95,24 +97,24 @@ const compareTokenTotals = (totals, spec, option) => {
       // But to add one we want a number, so we parse it then add one.
       // Why do we want to add one?
       totals[parseInt(key)] || toBN(0).gte(spec[key]),
-      `Mismatch for option ${option} guarantees[${key}]`,
+      `Mismatch for option ${option} guarantees[${key}]`
     );
   });
 };
 
-describe('DivaItemLootBox', () => {
+describe("DivaItemLootBox", () => {
   // To install the proxy mock and the attack contract we deploy our own
   // instances of all the classes here rather than using the ones that Truffle
   // deployed.
   before(async () => {
     /* Loading Contracts in this test */
-    const LootBoxRandomness = await ethers.getContractFactory('LootBoxRandomness');
+    const LootBoxRandomness = await ethers.getContractFactory("LootBoxRandomness");
     const lootBoxRandomness = await LootBoxRandomness.deploy();
 
-    const MockProxyRegistry = await ethers.getContractFactory('MockProxyRegistry');
-    const DivaItem = await ethers.getContractFactory('DivaItem');
-    const DivaItemFactory = await ethers.getContractFactory('DivaItemFactory');
-    const DivaItemLootBox = await ethers.getContractFactory('DivaItemLootBox', {
+    const MockProxyRegistry = await ethers.getContractFactory("MockProxyRegistry");
+    const DivaItem = await ethers.getContractFactory("DivaItem");
+    const DivaItemFactory = await ethers.getContractFactory("DivaItemFactory");
+    const DivaItemLootBox = await ethers.getContractFactory("DivaItemLootBox", {
       libraries: { LootBoxRandomness: lootBoxRandomness.address },
     });
 
@@ -128,100 +130,89 @@ describe('DivaItemLootBox', () => {
     factory = await DivaItemFactory.deploy(
       proxy.address,
       creatureAccessory.address,
-      lootBox.address,
+      lootBox.address
     );
     await setup.setupAccessory(creatureAccessory, owner.address);
-    await creatureAccessory.connect(owner).setApprovalForAll(
-      factory.address,
-      true,
-    );
+    await creatureAccessory.connect(owner).setApprovalForAll(factory.address, true);
     await creatureAccessory.transferOwnership(factory.address);
     await setup.setupAccessoryLootBox(lootBox, factory);
   });
 
   // Calls _mint()
-  describe('#mint()', () => {
-    it('should work for owner()', async () => {
+  describe("#mint()", () => {
+    it("should work for owner()", async () => {
       const option = toBN(vals.LOOTBOX_OPTION_BASIC);
       const amount = toBN(1);
 
-      await expect(
-        lootBox
-          .connect(owner)
-          .mint(userB.address, option, amount, '0x00'),
-      ).to.emit(lootBox, 'TransferSingle').withArgs(
-        owner.address,
-        testVals.ADDRESS_ZERO,
-        userB.address,
-        option,
-        amount,
-      );
+      await expect(lootBox.connect(owner).mint(userB.address, option, amount, "0x00"))
+        .to.emit(lootBox, "TransferSingle")
+        .withArgs(owner.address, testVals.ADDRESS_ZERO, userB.address, option, amount);
     });
 
-    it('should work for proxy', async () => {
+    it("should work for proxy", async () => {
       const option = vals.LOOTBOX_OPTION_BASIC;
       const amount = toBN(1);
       await expect(
-        lootBox
-          .connect(proxyForOwner)
-          .mint(userB.address, option, amount, '0x00'),
-      ).to.emit(lootBox, 'TransferSingle').withArgs(
-        proxyForOwner.address,
-        testVals.ADDRESS_ZERO,
-        userB.address,
-        option,
-        amount,
-      );
+        lootBox.connect(proxyForOwner).mint(userB.address, option, amount, "0x00")
+      )
+        .to.emit(lootBox, "TransferSingle")
+        .withArgs(
+          proxyForOwner.address,
+          testVals.ADDRESS_ZERO,
+          userB.address,
+          option,
+          amount
+        );
     });
 
-    it('should not be callable by non-owner() and non-proxy', async () => {
+    it("should not be callable by non-owner() and non-proxy", async () => {
       const amount = toBN(1);
       await expect(
         lootBox
           .connect(userB)
-          .mint(userB.address, vals.LOOTBOX_OPTION_PREMIUM, amount, '0x00'),
-      ).to.be.revertedWith('Lootbox: owner or proxy only');
+          .mint(userB.address, vals.LOOTBOX_OPTION_PREMIUM, amount, "0x00")
+      ).to.be.revertedWith("Lootbox: owner or proxy only");
     });
 
-    it('should not work for invalid option', async () => {
+    it("should not work for invalid option", async () => {
       const amount = toBN(1);
       await expect(
         lootBox
           .connect(owner)
-          .mint(userB.address, vals.NO_SUCH_LOOTBOX_OPTION, amount, '0x00'),
-      ).to.be.revertedWith('Lootbox: Invalid Option');
+          .mint(userB.address, vals.NO_SUCH_LOOTBOX_OPTION, amount, "0x00")
+      ).to.be.revertedWith("Lootbox: Invalid Option");
     });
 
-    // describe('#unpack()', () => {
-    //   it('should mint guaranteed class amounts for each option', async () => {
-    //     for (let i = 0; i < vals.NUM_LOOTBOX_OPTIONS; i++) {
-    //       const option = vals.LOOTBOX_OPTIONS[i];
-    //       const amount = toBN(1);
-    //       // console.log(i);
-    //       await lootBox
-    //         .connect(proxyForOwner)
-    //         .mint(userB.address, option, amount, '0x00');
+    describe("#unpack()", () => {
+      it("should mint guaranteed class amounts for each option", async () => {
+        for (let i = 0; i < 1; i++) {
+          const option = vals.LOOTBOX_OPTIONS[i];
+          const amount = toBN(2);
+          // console.log(i);
 
-    //       const tx = await lootBox
-    //         .connect(userB)
-    //         .unpack(userB.address, option, amount);
+          await lootBox
+            .connect(proxyForOwner)
+            .mint(userB.address, option, amount, "0x00");
 
-    //       let receipt = await tx.wait();
+          const tx = await lootBox.connect(userB).unpack(option, userB.address, amount);
 
-    //       console.log(receipt);
-    //       // expect(receipt)
-    //       //   .to.emit(receipt, 'LootBoxOpened').withArgs(
-    //       //     userB.address,
-    //       //     userB.address,
-    //       //     toBN(option),
-    //       //     toBN(vals.LOOTBOX_OPTION_AMOUNTS[option]),
-    //       //   );
-    //       const totals = totalEventTokens(receipt, userB);
-    //       assert.ok(totals.total.eq(toBN(vals.LOOTBOX_OPTION_AMOUNTS[option])));
-    //       compareTokenTotals(totals, vals.LOOTBOX_OPTION_GUARANTEES[option], option);
-    //     }
-    //   });
-    // });
+          let receipt = await tx.wait();
+
+          console.log(receipt);
+          console.log(receipt.events);
+          // expect(receipt)
+          //   .to.emit(receipt, 'LootBoxOpened').withArgs(
+          //     userB.address,
+          //     userB.address,
+          //     toBN(option),
+          //     toBN(vals.LOOTBOX_OPTION_AMOUNTS[option]),
+          //   );
+          // const totals = totalEventTokens(receipt, userB);
+          // assert.ok(totals.total.eq(toBN(vals.LOOTBOX_OPTION_AMOUNTS[option])));
+          // compareTokenTotals(totals, vals.LOOTBOX_OPTION_GUARANTEES[option], option);
+        }
+      });
+    });
   });
 });
 
@@ -252,7 +243,7 @@ describe('DivaItemLootBox', () => {
 //             itemsMinted: toBN(vals.LOOTBOX_OPTION_AMOUNTS[option])
 //           }
 //         );
-// 
+//
 //         const totals = totalEventTokens(receipt, userB);
 //         assert.ok(totals.total.eq(toBN(vals.LOOTBOX_OPTION_AMOUNTS[option])));
 //         compareTokenTotals(totals, vals.LOOTBOX_OPTION_GUARANTEES[option], option);
