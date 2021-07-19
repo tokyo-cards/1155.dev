d0508107c3a5b3060da7ec4d579c001669dd571f
void-anderson <anderson@diva.cards>
2021-07-19 16:16:27 +0900

    lint

--- hardhat.config.js
+++ hardhat.config.js
@@ -6,12 +6,12 @@ let secrets;
 try {
   secrets = require('./secrets');
 } catch (error) {
-  secrets = require('./example.secrets')
+  secrets = require('./example.secrets');
 }
 
 // This is a sample Hardhat task. To learn how to create your own go to
 // https://hardhat.org/guides/create-task.html
-task("accounts", "Prints the list of accounts", async () => {
+task('accounts', 'Prints the list of accounts', async () => {
   const accounts = await ethers.getSigners();
 
   for (const account of accounts) {
@@ -33,30 +33,30 @@ module.exports = {
     disambiguatePaths: false,
   },
   solidity: {
-    version:  "0.8.4",
+    version: '0.8.4',
     settings: {
-     optimizer: {
-       enabled: true,
-       runs: 1000,
-     },
-    }
+      optimizer: {
+        enabled: true,
+        runs: 1000,
+      },
+    },
   },
   networks: {
     hardhat: {
       throwOnTransactionFailures: true,
       throwOnCallFailures: true,
       allowUnlimitedContractSize: true,
-      blockGasLimit: 6721975 
+      blockGasLimit: 6721975,
     },
     rinkeby: {
-      url: secrets.rinkeby.alchemy_url, 
+      url: secrets.rinkeby.alchemy_url,
       accounts: {
-        mnemonic: secrets.rinkeby.mnemonic
+        mnemonic: secrets.rinkeby.mnemonic,
       },
       allowUnlimitedContractSize: true,
       throwOnCallFailures: true,
       throwOnTransactionFailures: true,
-      blockGasLimit: 6721975 
-    }
-  }
+      blockGasLimit: 6721975,
+    },
+  },
 };
